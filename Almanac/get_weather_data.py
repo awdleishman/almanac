cbcee8254b0f15a13f8afce6f90c9bfa12e50e61
Allan Leishman <84040340+awdleishman@users.noreply.github.com>
2023-10-06 13:36:57 -0500

    Applying linting to .py files

--- Almanac/get_weather_data.py
+++ Almanac/get_weather_data.py
@@ -1,6 +1,4 @@
 import requests
-import pandas as pd
-import numpy as np
 import meteostat as ms
 from datetime import datetime
 from string import punctuation
@@ -8,21 +6,25 @@ from string import punctuation
 
 def get_weather_data(place, start, end=datetime.now()):
     """
-     A function for getting daily weather data for a particular location from start date to end date.
+     A function for getting daily weather data for a particular location from
+     start date to end date.
 
      Parameters:
 
      place : str
-         A string containing the common name of the location to fetch weather data for.
+         A string containing the common name of the location to fetch weather
+         data for.
 
          Examples: 'Chicago, IL', 'South Bend, IN, 46617, US', 'Paris, France'
 
      start : str | datetime
-         A string containing the start date to fetch weather data for. Must be in the form %Y-%m-%d. Example: '2021-12-25'.
+         A string containing the start date to fetch weather data for. Must be
+         in the form %Y-%m-%d. Example: '2021-12-25'.
          This may also be a datetime object.
 
      end : str | datetime, optional (Defaults to the current date)
-         A string containing the end date to fetch weather data for. Must be in the form %Y-%m-%d. Example: '2021-12-25'.
+         A string containing the end date to fetch weather data for. Must be
+         in the form %Y-%m-%d. Example: '2021-12-25'.
          This may also be a datetime object.
 
 
@@ -46,7 +48,8 @@ def get_weather_data(place, start, end=datetime.now()):
     # Base url for searching geocode.maps.co
     base_url = "https://geocode.maps.co/search?q="
 
-    # Remove punctuation from place and replace spaces with + for the search url
+    # Remove punctuation from place and replace spaces with + for the search
+    # url
     place_no_punc = ""
 
     for char in place:
@@ -58,7 +61,8 @@ def get_weather_data(place, start, end=datetime.now()):
     # Build search URL from base URL and place
     url = base_url + place_url
 
-    # Request place information from the created url. The information is returned as a dict assigned as place_info
+    # Request place information from the created url. The information is
+    # returned as a dict assigned as place_info
     r = requests.get(url)
     place_info = r.json()[0]
 
@@ -66,11 +70,12 @@ def get_weather_data(place, start, end=datetime.now()):
     lat = float(place_info["lat"])
     lon = float(place_info["lon"])
 
-    # Make sure start and end are of type = datetime. This needs work to accept more date formats that may be entered
-    if type(start) == str:
+    # Make sure start and end are of type = datetime. This needs work to
+    # accept more date formats that may be entered
+    if start is str:
         start = datetime.strptime(start, "%Y-%m-%d")
 
-    if type(end) == str:
+    if end is str:
         end = datetime.strptime(end, "%Y-%m-%d")
 
     location = ms.Point(lat, lon)
