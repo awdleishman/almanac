85c4f99207401938fc560e5af7cc629f2915c437
Allan Leishman <84040340+awdleishman@users.noreply.github.com>
2023-10-06 13:48:42 -0500

    Setting up pre-commit checks (#11)
    * Adding pre-commit hooks
    
    * Black formatting existing files
    
    * Updating README
    
    * Adding Flake8 linting check
    
    * Applying linting to .py files

--- Almanac/get_weather_data.py
+++ Almanac/get_weather_data.py
@@ -1,33 +1,35 @@
 import requests
-import pandas as pd
-import numpy as np
 import meteostat as ms
 from datetime import datetime
 from string import punctuation
 
-def get_weather_data(place,start,end=datetime.now()):
-    
-    '''
-     A function for getting daily weather data for a particular location from start date to end date.
-     
+
+def get_weather_data(place, start, end=datetime.now()):
+    """
+     A function for getting daily weather data for a particular location from
+     start date to end date.
+
      Parameters:
-     
+
      place : str
-         A string containing the common name of the location to fetch weather data for.
-         
+         A string containing the common name of the location to fetch weather
+         data for.
+
          Examples: 'Chicago, IL', 'South Bend, IN, 46617, US', 'Paris, France'
-         
+
      start : str | datetime
-         A string containing the start date to fetch weather data for. Must be in the form %Y-%m-%d. Example: '2021-12-25'.
+         A string containing the start date to fetch weather data for. Must be
+         in the form %Y-%m-%d. Example: '2021-12-25'.
          This may also be a datetime object.
-         
+
      end : str | datetime, optional (Defaults to the current date)
-         A string containing the end date to fetch weather data for. Must be in the form %Y-%m-%d. Example: '2021-12-25'.
-         This may also be a datetime object. 
-        
-        
+         A string containing the end date to fetch weather data for. Must be
+         in the form %Y-%m-%d. Example: '2021-12-25'.
+         This may also be a datetime object.
+
+
     Returns:
-    
+
     data : pandas.DataFrame
         A data frame containing the following fields:
             time: The date : datetime
@@ -41,47 +43,43 @@ def get_weather_data(place,start,end=datetime.now()):
             wpgt: The peak wind gust in km/h : float
             pres: The average sea-level air pressure in hPa : float
             tsun: The daily sunshine total in minutes : float
-    '''
-    
-    
-    #Base url for searching geocode.maps.co
-    base_url = 'https://geocode.maps.co/search?q='
-    
-    
-    #Remove punctuation from place and replace spaces with + for the search url
-    place_no_punc = ''
+    """
+
+    # Base url for searching geocode.maps.co
+    base_url = "https://geocode.maps.co/search?q="
+
+    # Remove punctuation from place and replace spaces with + for the search
+    # url
+    place_no_punc = ""
 
     for char in place:
         if char not in punctuation:
-            place_no_punc = place_no_punc+char
-            
-    place_url = place_no_punc.replace(' ','+')
-    
-    #Build search URL from base URL and place
+            place_no_punc = place_no_punc + char
+
+    place_url = place_no_punc.replace(" ", "+")
+
+    # Build search URL from base URL and place
     url = base_url + place_url
-    
-    
-    #Request place information from the created url. The information is returned as a dict assigned as place_info
+
+    # Request place information from the created url. The information is
+    # returned as a dict assigned as place_info
     r = requests.get(url)
     place_info = r.json()[0]
-    
-    #Extract the lat and lon info from place_info
-    lat = float(place_info['lat'])
-    lon = float(place_info['lon'])
-   
 
+    # Extract the lat and lon info from place_info
+    lat = float(place_info["lat"])
+    lon = float(place_info["lon"])
 
-    
-    #Make sure start and end are of type = datetime. This needs work to accept more date formats that may be entered
-    if type(start)==str:
-        start = datetime.strptime(start,'%Y-%m-%d')
-        
-    if type(end)==str:
-        end = datetime.strptime(end,'%Y-%m-%d')
-        
-    
-    location = ms.Point(lat,lon)
-    
-    data = ms.Daily(location,start,end)
+    # Make sure start and end are of type = datetime. This needs work to
+    # accept more date formats that may be entered
+    if start is str:
+        start = datetime.strptime(start, "%Y-%m-%d")
+
+    if end is str:
+        end = datetime.strptime(end, "%Y-%m-%d")
+
+    location = ms.Point(lat, lon)
+
+    data = ms.Daily(location, start, end)
     data = data.fetch()
     return data
--- README.md
+++ README.md
@@ -3,5 +3,9 @@
 ## Environment Setup
 To install environment, run:
 `conda env create  --file conda.yml`
+
 `conda activate almanac-env`
+
 `python -m ipykernel install --user --name almanac-kernel --display-name "almanac kernel"`
+
+`pre-commit install`
